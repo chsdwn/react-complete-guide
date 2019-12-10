fdfadc3f3f174defc4165c60cb58506b03a4444e
chsdwn <hulusikafalier@gmail.com>
2019-12-10 19:53:27 +0300

    Class and function based components. props and state usage.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
+import Practice1 from './Practice1/Practice1';
 
-function App() {
+/* function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi from emmet</h1>
     </div>
   );
+}*/
+
+/* const App = (props) => {
+  // There is a new version to handle states like this but companies use this method rn.
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'Selim' },
+      { name: 'Mehmet' },
+    ]
+  });
+
+  const [ otherState, setOtherState ] = useState({
+    otherState: 'other state'
+  })
+
+  const switchNameHandler = (newName) => {
+    //this.state.persons[0].name = 'Ali';
+    setPersonsState(
+      {
+        persons: [
+          { name: newName },
+          { name: 'Veli' }
+        ]
+      }
+    );
+  };
+
+  const nameChangedHandler = (event) => {
+    setPersonsState(
+      {
+        persons: [
+          { name: event.target.value },
+          { name: 'Veli' }
+        ]
+      }
+    );
+  }
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  }
+
+  //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'hello from createElement'));
+  return (
+    <div className="App">
+      <h1>Hello from class</h1>
+      <p>A working p</p>
+      <button style={style} onClick={() => switchNameHandler('Mahmut')}>Switch Name</button>  Not Recommended, Inefficient
+      <Person name="Hulu" />
+      <Person name="Hulusi" click={switchNameHandler.bind(this, 'Halil')}>Hobbies: Programming</Person>  Use this method
+      <Person name={personsState.persons[0].name} changed={nameChangedHandler} />
+      <Person name={personsState.persons[1].name} />
+    </div>
+  );
+} */
+
+class App extends React.Component {
+  render() {
+    return (
+      <Practice1 />
+    );
+  }
 }
 
 export default App;
