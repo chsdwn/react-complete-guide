f618ce0611bf577d4ea1873821a5dcf72dae93a7
chsdwn <hulusikafalier@gmail.com>
2019-12-25 20:10:15 +0300

    Higher Order Components (HOC) - Introduction.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,11 +5,11 @@ import Aux from '../../../hoc/Auxiliary';
 class Person extends Component {
   render() {
     return (
-      <React.Fragment>
+      <Aux>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </React.Fragment>
+      </Aux>
     );
   }
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props) {
@@ -75,7 +76,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => {
           this.setState({showCockpit: false });
           }}>Remove Cockpit</button>
@@ -86,7 +87,7 @@ class App extends Component {
           clicked={this.togglePersonsHandler}/>
         ) : null}
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
